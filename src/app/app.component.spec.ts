9911c4b2dcdcbbb820fd7cf9a012955512bfe1fa
Jorge Luiz <44803139+jorgelro@users.noreply.github.com>
2024-10-28 22:47:27 -0300

    fix app component unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { Store } from '@ngrx/store';
-import { MockStore } from '@ngrx/store/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
     declarations: [AppComponent],
+    providers: [
+      provideMockStore({}),
+      HttpClientTestingModule,
+      HttpClient,
+      HttpHandler
+    ],
+    schemas: [NO_ERRORS_SCHEMA]
   }));
 
   it('should create the app', () => {
